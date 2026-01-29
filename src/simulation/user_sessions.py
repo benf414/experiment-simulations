d4f65c11f6dc74003dac934dec328116a20dc621
Ben Free <255447229+benf414@users.noreply.github.com>
2026-01-29 05:08:06 -0600

    Added and refigured experiments

--- src/simulation/user_sessions.py
+++ src/simulation/user_sessions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def user_weekly_session_dist(alpha=2.5, theta=1):
+def user_weekly_session_dist(alpha=3, theta=1):
     """
     Samples from a gamma distribution to find the poisson lambda parameter.
     Mean value is alpha * theta.
@@ -17,31 +17,48 @@ def user_weekly_sessions(lam: float):
     sessions = np.random.poisson(lam)
     return sessions
 
-def simulate_users(n_users: int, time_periods: int, exp_effect=0.0):
+def simulate_users(n_users: int, time_periods: int, sigma=.2, exp_effect=0.0):
     """
     Simulates multiple users over multiple time periods.
-    Adds an experimental effect to the treatment group in the post-experiment period.
+    Creates pre-experiment periods for each post-experiment period.
+    Starts at initial lambda value then applies random walk with standard deviation sigma.
+    Experiment effects are applied to treatment group in post-experiment periods.
     Half of the users are assigned to control and half to treatment.
-    Half of the time periods are considered pre-experiment and half post-experiment.
-    Returns 4 arrays of shape (n_users, time_periods/2) with the number of sessions for each user in each time period.
     """
     if n_users % 2 != 0:
         raise ValueError("n_users must be an even number to split into control and treatment groups")
-    if time_periods % 2 != 0:
-        raise ValueError("time_periods must be an even number to split into pre and post experiment periods")
 
-    pre_c_user_sessions = np.zeros((n_users//2, time_periods//2), dtype=int)
-    post_c_user_sessions = np.zeros((n_users//2, time_periods//2), dtype=int)
-    pre_t_user_sessions = np.zeros((n_users//2, time_periods//2), dtype=int)
-    post_t_user_sessions = np.zeros((n_users//2, time_periods//2), dtype=int)
+    pre_c_user_sessions = np.zeros((n_users//2, time_periods), dtype=int)
+    post_c_user_sessions = np.zeros((n_users//2, time_periods), dtype=int)
+    pre_t_user_sessions = np.zeros((n_users//2, time_periods), dtype=int)
+    post_t_user_sessions = np.zeros((n_users//2, time_periods), dtype=int)
 
-    for i in range(n_users//2):
-        lam = user_weekly_session_dist()
-        lam_affected = user_weekly_session_dist(theta=1*(1+exp_effect))
-        for t in range(time_periods//2):
-            pre_c_user_sessions[i, t] = user_weekly_sessions(lam)
-            post_c_user_sessions[i, t] = user_weekly_sessions(lam)
-            pre_t_user_sessions[i, t] = user_weekly_sessions(lam)
-            post_t_user_sessions[i, t] = user_weekly_sessions(lam_affected)
+    for g in ['control', 'treatment']:
+        for i in range(n_users//2):
+            lam = user_weekly_session_dist()
+            lam_max = lam * 2
+            lam_min = min(0, lam * 0.33)
+            for t in range(time_periods):
+                lam = lam * (1 + np.random.normal(0, sigma))
+                lam = max(lam_min, min(lam_max, lam))
+
+                if g == 'control':
+                    pre_c_user_sessions[i, t] = user_weekly_sessions(lam)
+                else:
+                    pre_t_user_sessions[i, t] = user_weekly_sessions(lam)
+
+            if g == 'treatment':
+                lam = lam * (1 + exp_effect)
+            lam_max = lam * 2
+            lam_min = min(0, lam * 0.33)
+            
+            for t in range(time_periods):
+                lam = lam * (1 + np.random.normal(0, sigma))
+                lam = max(lam_min, min(lam_max, lam))
+
+                if g == 'control':
+                    post_c_user_sessions[i, t] = user_weekly_sessions(lam)
+                else:
+                    post_t_user_sessions[i, t] = user_weekly_sessions(lam)
     
     return pre_c_user_sessions, post_c_user_sessions, pre_t_user_sessions, post_t_user_sessions
